03053af2f0b7b3334f4e977d379f1eef9c879e82
Cireimu <ethanhoover3@gmail.com>
2020-07-14 08:57:17 -0700

    created basic get single user book by id route with validation

--- api/middleware/userBooks/userBooksCheckFor.js
+++ api/middleware/userBooks/userBooksCheckFor.js
@@ -1,11 +1,11 @@
-const UserBooks = require('../models/userBooksModel');
+const UserBooks = require('../../models/userBooksModel');
 
 function checkForUserBooks(req, res, next) {
   UserBooks.findAll().then((userBooks) => {
     if (userBooks.length != 0) {
       next();
     } else {
-      res.status(404).json({ erorr: 'No user books were found' });
+      res.status(404).json({ message: 'No user books were found' });
     }
   });
 }
--- api/routes/userBooks.js
+++ api/routes/userBooks.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authRequired = require('../middleware/authRequired');
-const checkForUserBooks = require('../middleware/userBooksCheckFor');
+const checkForUserBooks = require('../middleware/userBooks/userBooksCheckFor');
+const checkForSingleUserBook = require('../middleware/userBooks/checkForSingleUserBook');
 const UserBooks = require('../models/userBooksModel');
 const router = express.Router();
 
@@ -12,8 +13,20 @@ router.get('/', checkForUserBooks, (req, res) => {
     .catch((err) => {
       res
         .status(500)
-        .json({ message: 'Server failed to retrieve user books', err });
+        .json({ error: 'Server failed to retrieve user books', err });
     });
 });
 
+router.get('/:bookId', checkForSingleUserBook, (req, res) => {
+  const { bookId } = req.params;
+  UserBooks.findById(bookId)
+    .then((userBook) => {
+      res.status(200).json(userBook);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: 'Server failed to retrieve user book', err });
+    });
+});
 module.exports = router;
